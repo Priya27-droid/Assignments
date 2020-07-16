536005c3a1b4c9350b60607d0e7bba34b0859d32
Priya27-droid <54000168+Priya27-droid@users.noreply.github.com>
2020-07-16 20:03:46 +0530

    Created MVC Model for Product

---
+++ Assignment1_MVC/ProductApp/ProductApp/ProductApp/Models/ProductList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ProductApp.Models
+{
+    public class ProductList
+    {
+        public List<Product> Products { get; set; }
+        public ProductList()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
