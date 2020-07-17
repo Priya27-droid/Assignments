099b95b1b456e05dee2f651255c0c115825f053a
Priya27-droid <54000168+Priya27-droid@users.noreply.github.com>
2020-07-17 15:49:27 +0530

    Created MVC Core App with CRUD operations using EF Core

---
+++ Assignment2_EF/Assignment2/Repositories/CustomerRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment2.Entities;
+
+namespace Assignment2.Repositories
+{
+    public class CustomerRepository
+    {
+        public CustomerContext db = new CustomerContext();
+        public List<Customer> GetCustomer()
+        {
+            return db.Customers.ToList(); 
+            
+        }
+        public Customer GetCustomerById(string cid)
+        {
+            return db.Customers.Find(cid); 
+        }
+        public void Add(Customer c)
+        {
+            db.Customers.Add(c); 
+            db.SaveChanges();
+        }
+        public void Delete(Customer c)
+        {
+            db.Customers.Remove(c); 
+            db.SaveChanges();
+        }
+        public void Update(Customer c)
+        {
+            db.Customers.Update(c); 
+            db.SaveChanges();
+        }
+    }
+}
